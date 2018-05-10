07dbf09123b1cfd0a172bbb5c115f5f8e2aaa8ce
hasan330 <hasan.saleh.khader@gmail.com>
2018-05-10 18:02:27 +0300

    Add more cases for swithc statement

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,14 +8,15 @@ export function calculateOhda(currentOhdaValue, startingMoney, convertedMoneyArr
             if (currentOhdaValue < ohdaValue) {
                 currentOhdaValue += convertedMoneyArray[i]
             } else {
-                let noteValue= keys[i - 1];
+                let noteValue = keys[i - 1];
                 console.log("Note value is: ", noteValue)
                 let diffAmount = getDiff(ohdaValue, currentOhdaValue, noteValue);
                 
                 moneyToBeSubmitted[noteValue] = diffAmount;
-                
-                moneyToBeKept[noteValue] = startingMoney[noteValue] - diffAmount;
-                convertedMoneyArray[5] = convertedMoneyArray[5] - (noteValue * diffAmount) 
+                moneyToBeKept[noteValue]      = startingMoney[noteValue] - diffAmount;
+
+                let index                  = getArrayIndexFromNoteValue(noteValue);
+                convertedMoneyArray[index] = convertedMoneyArray[index] - (noteValue * diffAmount) 
 
                 console.log("Updated convertedMoneyArray ", convertedMoneyArray);
                 console.log("Updated moneyToBeKept ", moneyToBeKept);
@@ -26,12 +27,43 @@ export function calculateOhda(currentOhdaValue, startingMoney, convertedMoneyArr
                 break;
             }
         }
-
     }
-
-   
 }
 
 export function getDiff(ohdaValue, currentValue, noteValue){
 	return Math.floor((currentValue - ohdaValue) / noteValue);
+}
+
+
+function getArrayIndexFromNoteValue(noteValue) {
+    switch (noteValue) {
+        case 200:
+            return 7;
+            break;
+        case 100:
+            return 6;
+            break;
+         case 50:
+            return 5;
+            break;
+        case 20:
+            return 4;
+            break;
+        case 10:
+            return 3;
+            break;
+        case 5:
+            return 2;
+            break;
+        case 2:
+            return 1;
+            break;
+        case 1:
+            return 0;
+            break;
+        
+        default:
+            // statements_def
+            break;
+    }
 }
